463e8247736010e87e95ccfdc92e1a0c540e3b4a
GTAMaple <83424380+GTAMaple@users.noreply.github.com>
2021-10-23 21:51:04 -0400

    Create test.js

---
+++ test.js
@@ -0,0 +1,12 @@
+
+let obj = {
+  name: 'Bonnie',
+  age: 27,
+  education: 'Engineer'
+};
+
+console.log(obj)
+
+obj.college = 'College AB';
+console.log('After adding a property using dot syntax');
+console.log(obj);
